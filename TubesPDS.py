e600c11e42419007d92714ab946761594cb04987
Michael <6181801041@student.unpar.ac.id>
2021-12-11 13:07:29 +0700

    Update TubesPDS.py

--- TubesPDS.py
+++ TubesPDS.py
@@ -1,10 +1,16 @@
+#Import Library
 import pandas as pd
 import io
 import requests
+import matplotlib.pyplot as plt
 
 #Read Data
 url = ''
 s=requests.get(url).content
 df=pd.read_csv(io.StringIO(s.decode('utf-8')))
+
+
+# Checking the missing values
+df.isnull().sum()
 
 #Explore Data
