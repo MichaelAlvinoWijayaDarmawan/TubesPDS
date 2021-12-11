d9dd28056c5427e840ceba3128684d840654e4fc
yalvih <54298830+yalvih@users.noreply.github.com>
2021-12-11 13:53:32 +0700

    Mengganti file dan penambahan yang sudah pisah

--- TubesPDS.py
+++ TubesPDS.py
@@ -9,8 +9,12 @@ url = 'https://data.cityofnewyork.us/api/views/825b-niea/rows.csv?accessType=DOW
 s = requests.get(url).content
 df = pd.read_csv(io.StringIO(s.decode('utf-8')))
 
-
 # Checking the missing values
 df.isnull().sum()
 
-#Explore Data
+#melakukan penyiapan data dengan melakukan data cleaning. In case ada data yang kotor atau data yang outliers
+#Karena grades ada yang byukan angka (ALL GRADES) -- ALLGRADES PERLU DIPISAH KE TABEL TERSENDIRI
+df_allGrades = df.loc[df['Grade'] == 'All Grades']
+
+#generate tabel bukan all grades
+df_nonAllGrades = df.loc[df['Grade'] != 'All Grades']
